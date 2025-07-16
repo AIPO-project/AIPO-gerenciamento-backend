fbf20f92cfb2f36181d31b3e0de5a774e9c868c8
ItsNabru <128171489+ItsNabru@users.noreply.github.com>
2025-07-16 17:20:28 -0300

    Adição de Rotas para busca de salas

--- app.py
+++ app.py
@@ -59,6 +59,48 @@ def get_user(matricula):
     except:
         return jsonify({'message': 'Usuário não encontrado'}), 404
 
+@app.route("/api/salas", methods=['GET'])
+@jwt_required()
+def get_salas():
+    try:
+        conexaoBD()
+        list = []
+        salas = read("SELECT * FROM salas", None)
+        for sala in salas: 
+            data = {
+                'nome': sala['nome'],
+                'codigo': sala['codigo'],
+                'fechadura': sala['fechadura'],
+                'local': sala['local'],
+                'ativo': sala['ativo']
+            }
+            list.append(data)
+
+        return jsonify(list)
+    except:
+        return jsonify({'message': 'Não há salas cadastradas'}), 404
+
+@app.route("/api/salas/<matricula>", methods=['GET'])
+@jwt_required()
+def get_salasPorUsuario(matricula):
+    try:
+        conexaoBD()
+        list = []
+        salas = read("SELECT s.nome, s.id, s.local, s.fechadura, s.codigo, s.ativo FROM salas AS s, usuarios AS u, autorizacao AS a WHERE s.id = a.id_sala AND u.id = a.id_usuario AND u.matricula = %s", (matricula,))
+        for sala in salas: 
+            data = {
+                'nome': sala['nome'],
+                'codigo': sala['codigo'],
+                'fechadura': sala['fechadura'],
+                'local': sala['local'],
+                'ativo': sala['ativo']
+            }
+            list.append(data)
+
+        return jsonify(list)
+    except:
+        return jsonify({'message': 'Não há salas cadastradas para o usuario'}), 404
+
 @app.route("/api/login", methods=['GET', 'POST'])
 def login():
     data = request.json
